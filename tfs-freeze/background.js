aff4562d6058afeaa33b9548d3c1286a3ef29baf
Abel Gancsos <agancsos@users.noreply.github.com>
2018-06-20 10:48:19 -0400

    Made extension compatible with Chrome API

--- tfs-freeze/background.js
+++ tfs-freeze/background.js
@@ -1,43 +1,34 @@
 function toggleFreeze() {
-    var badgeTextObject = browser.browserAction.getBadgeText({});
-    badgeTextObject.then(extractBadgeText);    
-}
-
-function extractBadgeText(text) {
-    if(text == "OFF" || text == "") {
-        browser.browserAction.setBadgeText({text : "ON"});
-        browser.browserAction.setBadgeBackgroundColor({color : "green"});
-        stopLoad();
-    }
-    else {
-        browser.browserAction.setBadgeText({text : "OFF"});
-        browser.browserAction.setBadgeBackgroundColor({color : "red"});
-        enableLoad();
-    }
-
+    var badgeText = chrome.browserAction.getBadgeText({}, function(text){
+        if(text == "OFF" || text == "") {
+            chrome.browserAction.setBadgeText({text : "ON"});
+            chrome.browserAction.setBadgeBackgroundColor({color : "green"});
+            stopLoad();
+        }
+        else {
+            chrome.browserAction.setBadgeText({text : "OFF"});
+            chrome.browserAction.setBadgeBackgroundColor({color : "red"});
+            enableLoad();
+        }    
+    });
 }
 
 function stopLoad() {
     var myCode = "window.stop()";
-    var windowInfo = browser.tabs.executeScript({
+    var windowInfo = chrome.tabs.executeScript({
         code: myCode
       });
-      windowInfo.then(dummy);
 }
 
 function enableLoad() {
     var myCode = "location.reload()";
-    var windowInfo = browser.tabs.executeScript({
+    var windowInfo = chrome.tabs.executeScript({
         code: myCode
       });
-      windowInfo.then(dummy);
 }
 
-function dummy(result) {
-
-}
-browser.browserAction.onClicked.addListener(toggleFreeze);
+chrome.browserAction.onClicked.addListener(toggleFreeze);
 document.addEventListener("DOMContentLoaded", function(){
-    browser.browserAction.setBadgeText({text : "OFF"});
-    browser.browserAction.setBadgeBackgroundColor({color : "red"});
+    chrome.browserAction.setBadgeText({text : "OFF"});
+    chrome.browserAction.setBadgeBackgroundColor({color : "red"});
 });
